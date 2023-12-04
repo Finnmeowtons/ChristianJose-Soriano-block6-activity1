8a0d8b8357b940297b6b7a02a8780bbb0e2a3b0a
CJ Soriano <soriano.christianjose.m@gmail.com>
2023-12-04 15:49:30 +0800

    Added a button from activity_pink.xml and added its feature to go to MainActivity

--- app/src/main/java/com/soriano/christianjose/block6/p1/quiz/PinkActivity.kt
+++ app/src/main/java/com/soriano/christianjose/block6/p1/quiz/PinkActivity.kt
@@ -1,11 +1,20 @@
 package com.soriano.christianjose.block6.p1.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.soriano.christianjose.block6.p1.quiz.databinding.ActivityPinkBinding
 
 class PinkActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityPinkBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pink)
+        binding = ActivityPinkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnMainActivity.setOnClickListener {
+            val intent = Intent(this@PinkActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
