2d42aff50f729b8f9b343de11eff26ad019d0de9
CJ Soriano <soriano.christianjose.m@gmail.com>
2023-12-04 15:43:13 +0800

    Added PinkActivity Added button from activity_main.xml and added its feature to go to PinkActivity

--- app/src/main/java/com/soriano/christianjose/block6/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/soriano/christianjose/block6/p1/quiz/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.soriano.christianjose.block6.p1.quiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.soriano.christianjose.block6.p1.quiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //cj
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPinkActivity.setOnClickListener {
+            val intent = Intent(this@MainActivity, PinkActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/soriano/christianjose/block6/p1/quiz/PinkActivity.kt
@@ -0,0 +1,11 @@
+package com.soriano.christianjose.block6.p1.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PinkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pink)
+    }
+}
