efea2abb7a9d03d72dcb46a90909bc6bc64d6d39
CJ Soriano <soriano.christianjose.m@gmail.com>
2023-12-04 15:30:31 +0800

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ChristianJoseSoriano-Block6-Quiz"
+include(":app")
+ 
